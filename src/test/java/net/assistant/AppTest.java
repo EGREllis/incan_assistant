5f03f6bf707d1ea8a5e8ec59dcc9f0657ebab474
Edward Ellis <edwardellis.egre@gmail.com>
2021-01-20 08:40:33 +0000

    Maven archetype java project

---
+++ src/test/java/net/assistant/AppTest.java
@@ -0,0 +1,20 @@
+package net.assistant;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
