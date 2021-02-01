d128f63164235e51c55fd628b901ca3533fb5146
Edward Ellis <edwardellis.egre@gmail.com>
2021-02-01 14:22:40 +0000

    Cleaned up indices on DeckImpl

--- src/main/java/net/assistant/model/DeckImpl.java
+++ src/main/java/net/assistant/model/DeckImpl.java
@@ -3,24 +3,38 @@ package net.assistant.model;
 import java.util.Set;
 
 public class DeckImpl implements Deck {
+    public static final int HAZARD_ONE = -1;
+    public static final int HAZARD_TWO = -2;
+    public static final int HAZARD_THREE = -3;
+    public static final int HAZARD_FOUR = -4;
+    public static final int HAZARD_FIVE = -5;
+    public static final int ARTIFACT_ONE = 21;
+    public static final int ARTIFACT_TWO = 22;
+    public static final int ARTIFACT_THREE = 23;
+    public static final int ARTIFACT_FOUR = 24;
+    public static final int ARTIFACT_FIVE = 25;
+    private static final int POST_GEM_AND_HAZARD_DECK_INDEX = 30;
     private static final int[] cards = new int [] { 1, 2, 3, 4, 5,
                                                     5, 7, 7, 9, 11,
                                                     11, 13, 14, 15, 17,
-                                                    -1, -1, -1, -2, -2,
-                                                    -2, -3, -3, -3, -4,
-                                                    -4, -4, -5, -5, -5,
-                                                    21, 22, 23, 24, 25};
+                                                    HAZARD_ONE, HAZARD_ONE, HAZARD_ONE,
+                                                    HAZARD_TWO, HAZARD_TWO, HAZARD_TWO,
+                                                    HAZARD_THREE, HAZARD_THREE, HAZARD_THREE,
+                                                    HAZARD_FOUR, HAZARD_FOUR, HAZARD_FOUR,
+                                                    HAZARD_FIVE, HAZARD_FIVE, HAZARD_FIVE,
+                                                    ARTIFACT_ONE, ARTIFACT_TWO, ARTIFACT_THREE,
+                                                    ARTIFACT_FOUR, ARTIFACT_FIVE};
     private int[] deck;
     private int drawn = 0;
 
     public DeckImpl(int round, Set<Integer> seen) {
-        int size = 30 + round - seen.size();
+        int size = POST_GEM_AND_HAZARD_DECK_INDEX + round - seen.size();
         deck = new int[size];
         System.arraycopy(cards, 0, deck, 0, size);
 
         //TODO: Unit test this
         if (seen.size() > 0) {
-            int deckIndex = 30;
+            int deckIndex = POST_GEM_AND_HAZARD_DECK_INDEX;
             for (int cardIndex = 0; cardIndex < cards.length && deckIndex < deck.length; cardIndex++) {
                 if (!seen.contains(cards[30 + cardIndex])) {
                     deck[deckIndex++] = cards[30 + cardIndex];
